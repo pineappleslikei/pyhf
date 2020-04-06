2c1dd7c8b8c2f1087770d2a054edfdd51b9a29cd
Matthew Feickert <matthew.feickert@cern.ch>
2020-04-06 02:02:25 -0500

    Adopt the 'q' notation for consistency with backend library docs

--- src/pyhf/tensor/numpy_backend.py
+++ src/pyhf/tensor/numpy_backend.py
@@ -243,7 +243,7 @@ class numpy_backend:
     def exp(self, tensor_in):
         return np.exp(tensor_in)
 
-    def percentile(self, tensor_in, percentile, axis=None, interpolation="linear"):
+    def percentile(self, tensor_in, q, axis=None, interpolation="linear"):
         r"""
         Compute the :math:`q`-th percentile of the tensor along the specified axis.
 
@@ -259,7 +259,7 @@ class numpy_backend:
 
         Args:
             tensor_in (`tensor`): The tensor containing the data
-            percentile (`float` or `tensor`): The :math:`q`-th percentile to compute
+            q (`float` or `tensor`): The :math:`q`-th percentile to compute
             axis (`number` or `tensor`): The dimensions along which to compute
             interpolation (`str`): The interpolation method to use when the desired
                 percentile lies between two data points ``i < j``:
@@ -278,9 +278,7 @@ class numpy_backend:
             NumPy ndarray: The value of the :math:`q`-th percentile of the tensor along the specified axis.
 
         """
-        return np.percentile(
-            tensor_in, percentile, axis=axis, interpolation=interpolation
-        )
+        return np.percentile(tensor_in, q, axis=axis, interpolation=interpolation)
 
     def stack(self, sequence, axis=0):
         return np.stack(sequence, axis=axis)
